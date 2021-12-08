f02e2b3875116d61de2d465be6c3f393945994da
Hui Wang <88509047+hwang30git@users.noreply.github.com>
2021-12-08 17:23:44 -0500

    add Powers.java

---
+++ Powers.java
@@ -0,0 +1,11 @@
+public class Powers {
+  public static void main(String[] args) {
+    int i = 0;
+    int num = (int) Math.pow(2, i);
+    while (num < 694) {
+      System.out.println(num);
+      i++;
+      num = (int) Math.pow(2, i);
+    }
+  }
+}
